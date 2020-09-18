d76b95b00bcdbf3344408414ccef4f3d5c550f5f
mavrik12 <mavrik12@users.noreply.github.com>
2020-09-18 14:21:22 -0600

    gamev2.sh
    got a play area generating  with player moves using WASD.

--- gamev1.sh
+++ gamev1.sh
@@ -4,62 +4,75 @@
 #September 2020
 #DC801 & 801Labs
 
-#generates the map
-#Grid Var
-columns=24
-rows=6
+#size of player area
 
-i=0
-c=0
+#size of player area
+PLAYAREA_W=10
+PLAYAREA_H=20
+PLAYAREA_X=30
+PLAYAREA_Y=30
+
+# color codes
+RED=1
+GREEN=2
+YELLOW=3
+BLUE=4
+FUCHSIA=5
+CYAN=6
+WHITE=7
+
+colors=($RED $GREEN $YELLOW $BLUE $FUCHSIA $CYAN $WHITE)
 
 #player cord
-x=0
-y=0
+PLAYER_X=0
+PLAYER_Y=0
 
-# Game things
-LINE='|' # Builds the line of the text file
-END=0 # to break game loop
-while [[ $end -eq 0 ]]; do
-#DRAWS GAME MAP
-echo $x ',' $y>map.txt
-until [ $i -gt $rows ]; do #Generates the full row, by row
-  until [ $c -gt $columns ]; do  #Generates the cells on each row i.e. Columns
-    if [[ $i -eq 0 ]] #Top line of grid
-    then
-      LINE+='_'
-    elif [[ $i -eq $rows ]] #Bottom line of grid
-    then
-      LINE+='='
+#exit game loop
+exit=0
+
+#Controls pieces movien
+ticker() {
+  echo 'bla'
+}
+
+#Player inputes
+player_input () {
+  draw_playfield
+  read -rsn1 pmove
+  case $pmove in
+      w) PLAYER_Y=$(( PLAYER_Y - 1)) ;;
+      a) PLAYER_X=$(( PLAYER_X - 2)) ;;
+      s) PLAYER_Y=$(( PLAYER_Y + 1)) ;;
+      d) PLAYER_X=$(( PLAYER_X + 2)) ;;
+      e) echo "place" ;;
+      t) echo $PLAYER_X ',' $PLAYER_Y;; #trouble shoot show x and y
+      q) end=1 ;; #ends game
+      1)  echo -e "\e[4${colors[0]};31m  \e[00m" ;;
+      2)  echo -e "\e[4${colors[1]};31m  \e[00m" ;;
+      3)  echo -e "\e[4${colors[2]};31m  \e[00m" ;;
+      4)  echo -e "\e[4${colors[3]};31m  \e[00m" ;;
+    esac
+}
+
+draw_playfield () {
+  local i j x y line
+clear
+#rows
+for ((j = 0; j < $PLAYAREA_H; j += 1)) {
+  #columns
+  line=()
+  for ((i = 0; i < $PLAYAREA_W; i += 2 )) {
+    if [[ i -eq $PLAYER_X && j -eq $PLAYER_Y ]]; then
+      line+='[]'
     else
-      LINE+='-'
-  fi
-    c=$(( c + 1 ))
-  done
+      line+='--'
+    fi
+  }
+  echo $line
 
-  LINE+='|'
-  echo $LINE>>map.txt
-  LINE='|'
-  i=$(( i + 1 ))
-  c=0 #reset counter c to 0 for new line
+}
+}
+
+while [[ $end -eq 0 ]]; do
+  player_input
 done
-
-#player controlls
-read -rsn1 pmove
-case $pmove in
-    w) y=$(( y + 1)) ;;
-    a) x=$(( x - 1)) ;;
-    s) y=$(( y - 1)) ;;
-    d) x=$(( x + 1)) ;;
-    e) echo "place" ;;
-    t) echo $x ',' $y;; #trouble shoot show x and y
-    q) end=1 ;; #ends game
-  esac
-done
-
-#| |________________________
-#|-------------------------|
-#|-------------------------|
-#|-------------------------|
-#|-------------------------|
-#|-------------------------|
-#|=======================| |
