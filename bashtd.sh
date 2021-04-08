0d8588f09e9b977798defe0398720aae80d6ab99
Pexie <williamyocom@gmail.com>
2021-04-08 14:27:38 -0700

    Cosmetics, & trouble shooting
    Added:
    -Title draw
    -Padding
    -X,Y posision indicator
    -Started on player colion

--- bashtd.sh
+++ bashtd.sh
@@ -9,9 +9,11 @@
 #size of player area
 PLAYAREA_W=10
 PLAYAREA_H=20
-PLAYAREA_X=30
-PLAYAREA_Y=30
-
+#PLAYAREA_X=30
+#PLAYAREA_Y=30
+PLAYAREA_PADDING_L=10
+PLAYAREA_PADDING_R=18
+PLAYAREA_LINE=$((PLAYAREA_W+PLAYAREA_PADDING_L+PLAYAREA_PADDING_R))
 # color codes
 RED=1
 GREEN=2
@@ -47,20 +49,57 @@ player_input () {
       e) echo "place" ;;
       t) echo $PLAYER_X ',' $PLAYER_Y;; #trouble shoot show x and y
       q) end=1 ;; #ends game
-      1)  echo -e "\e[4${colors[0]};31m  \e[00m" ;;
-      2)  echo -e "\e[4${colors[1]};31m  \e[00m" ;;
-      3)  echo -e "\e[4${colors[2]};31m  \e[00m" ;;
-      4)  echo -e "\e[4${colors[3]};31m  \e[00m" ;;
     esac
+}
+
+
+#COLISION
+colision_player () {
+    #rows
+    echo "Line Width+Padding:"$PLAYAREA_LINE
+    echo "(X:"$PLAYER_X "Y:"$PLAYER_Y")" #shows player's X,Y in Grid
+    #columns
+}
+#colision_enemy () {}
+
+#PLAY AREA CREATION
+draw_title () {
+    line=()
+
+    ## First line
+    for ((j = 0; j < $PLAYAREA_LINE; j += 1)) { 
+    line+="=" 
+    }
+    echo $line
+    line=()
+
+    ## Secound Line
+    TITLE_X=$(((PLAYAREA_LINE-8)/2))
+    for ((j = 0; j < $TITLE_X; j += 1)) { 
+    line+="=" 
+    }
+    echo $line"|BASHTD|"$line
+    line=()
+
+    ## Third Line
+    for ((j = 0; j < $PLAYAREA_LINE; j += 1)) { 
+    line+="=" 
+    }
+    echo $line
 }
 
 draw_playfield () {
   local i j x y line
-clear
+clear                   #Clears screen
+#DRAW TITLE
+draw_title           #Draws title
+
+
+#DRAW PLAY AREA
 #rows
 for ((j = 0; j < $PLAYAREA_H; j += 1)) {
   #columns
-  line=()
+  line=()               #clears line
   for ((i = 0; i < $PLAYAREA_W; i += 2 )) {
     if [[ i -eq $PLAYER_X && j -eq $PLAYER_Y ]]; then
       line+='[]'
@@ -71,6 +110,8 @@ for ((j = 0; j < $PLAYAREA_H; j += 1)) {
   echo $line
 
 }
+#TEMP
+colision_player #Echoing line count with padding right now need to fix.
 }
 
 while [[ $end -eq 0 ]]; do
