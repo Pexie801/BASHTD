3d3dd4c7ee8f58fe145af4720bdac9ac68b3c24c
Pexie <williamyocom@gmail.com>
2021-04-09 14:10:18 -0700

    did a lot
    Added
    -Ticker
    -proto Enemy movement
    -Ticker timmer on screen
    
    Disabled movement for now

--- bashtd.sh
+++ bashtd.sh
@@ -4,36 +4,29 @@
 #September 2020
 #DC801 & 801Labs
 
-#size of player area
+#VARABILES
+  #size of player area
+  PLAYAREA_W=10
+  PLAYAREA_H=18
+  PLAYAREA_PADDING_L=10
+  PLAYAREA_PADDING_R=18
+  PLAYAREA_LINE=$((PLAYAREA_W+PLAYAREA_PADDING_L+PLAYAREA_PADDING_R))
 
-#size of player area
-PLAYAREA_W=10
-PLAYAREA_H=18
-PLAYAREA_PADDING_L=10
-PLAYAREA_PADDING_R=18
-PLAYAREA_LINE=$((PLAYAREA_W+PLAYAREA_PADDING_L+PLAYAREA_PADDING_R))
+  TEST_V=0
 
-# color codes
-RED=1
-GREEN=2
-YELLOW=3
-BLUE=4
-FUCHSIA=5
-CYAN=6
-WHITE=7
+  #player cord
+  PLAYER_X=0
+  PLAYER_Y=0
 
-colors=($RED $GREEN $YELLOW $BLUE $FUCHSIA $CYAN $WHITE)
+  #enemy cord
+  ENEMY_X=4
+  ENEMY_Y=3
 
-#player cord
-PLAYER_X=0
-PLAYER_Y=0
-
-#exit game loop
-exit=0
+  #exit game loop
+  exit=0
 
 #Player inputes
 player_input () {
-  draw_playfield
   read -rsn1 pmove
   case $pmove in
       w) if [[ PLAYER_Y -eq 0 ]] ; then
@@ -54,35 +47,25 @@ player_input () {
             PLAYER_Y=$(( PLAYER_Y + 1))
           fi ;;
 
-      d) #PLAYER_X=$(( PLAYER_X + 2)) 
-          if [[ PLAYER_X -eq $(( PLAYAREA_W - 2 )) ]] ; then
+      d)  if [[ PLAYER_X -eq $(( PLAYAREA_W - 2 )) ]] ; then
             PLAYER_X
           else
-            PLAYER_X=$(( PLAYER_X + 2))
+            PLAYER_X=$(( PLAYER_X + 2 ))
           fi ;;
       e) echo "place" ;;
-      t) echo $PLAYER_X ',' $PLAYER_Y;; #trouble shoot show x and y
       q) end=1 ;; #ends game
+      *)  continue
+          ;;
     esac
 }
-
-#player_move () {
- # if [[ PLAYER_Y -eq 0 ]] ; then
- # PLAYER_Y=$PLAYAREA_Y
- # else
- #PLAYER_Y=$(( PLAYER_Y - 1 ))
- # fi
-#}
-#COLISION
-colision_player () {
-    #rows
-    echo "Line Width+Padding:"$PLAYAREA_LINE
-    echo "(X:"$PLAYER_X "Y:"$PLAYER_Y")" #shows player's X,Y in Grid
-
-
-    #columns
+enemy_move () {
+  if [[ $ENEMY_Y -eq $PLAYAREA_H ]] ; then
+    ENEMY_Y=$((0))
+    ENEMY_X=$(( ENEMY_X - 2 ))
+  else
+    ENEMY_Y=$(( ENEMY_Y + 1 ))
+  fi
 }
-#colision_enemy () {}
 
 #PLAY AREA CREATION
 draw_title () {
@@ -111,31 +94,41 @@ draw_title () {
 }
 
 draw_playfield () {
-local i j x y line
-clear                   #Clears screen
-#DRAW TITLE
-draw_title           #Draws title
-
+local i j x y line tcount
+  clear                                                         # Clears screen
+  draw_title                                                    # Draws title
 
 #DRAW PLAY AREA
-#rows
-for ((j = 0; j < $PLAYAREA_H; j += 1)) {
-  #columns
-  line=()               #clears line
-  for ((i = 0; i < $PLAYAREA_W; i += 2 )) {
-    if [[ i -eq $PLAYER_X && j -eq $PLAYER_Y ]]; then
-      line+='[]'
-    else
-      line+='--'
-    fi
+  #ROWS
+  for ((j = 0; j < $PLAYAREA_H; j += 1)) {
+    #COLUMS
+    line=()                                                       # Clears line
+      for ((i = 0; i < $PLAYAREA_W; i += 2 )) {
+        if [[ i -eq $ENEMY_X && j -eq $ENEMY_Y ]]; then
+          line+='<>'
+        elif [[ i -eq $PLAYER_X && j -eq $PLAYER_Y ]]; then
+          line+='[]'
+        else
+          line+='--'
+        fi
+      }
+    echo $line
   }
-  echo $line
-
-}
-#TEMP
-colision_player #Echoing line count with padding right now need to fix.
+  echo "Line Width+Padding:"$PLAYAREA_LINE
+  echo "(X:"$PLAYER_X "Y:"$PLAYER_Y")"                        #shows player's X,Y in Grid 
+echo $TEST_V
 }
 
+g_count () {
+  sleep .5
+  TEST_V=$((TEST_V + 1))
+}
+
+######################################################################################
+################# Bits that run the game go here!!! ##################################
 while [[ $end -eq 0 ]]; do
-  player_input
+  #player_input                                                # Gets player input to move
+  enemy_move
+  draw_playfield                                              # Draws Play Field
+  g_count
 done
